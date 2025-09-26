acc6f7f4d9bc4e6c610364b60e3f8d1b0565df64
AngeloHO <angelo.oneda77@hotmail.com>
2025-09-26 13:48:54 -0300

    Fix: Adicionar include do arquivo de conexão no index.php

--- config/conexao.php
+++ config/conexao.php
@@ -17,18 +17,16 @@ function connect_local_mysqli($database = NULL, $charset = "utf8")
     if (mysqli_connect_errno()) {
         log_erro("Erro ao conectar ao banco de dados: "
             . mysqli_connect_error());
-        /* trigger_error("Erro ao conectar ao banco de dados: "
-          . mysqli_connect_error()); */
-    }
+        
+    };
 
     if ($charset <> '') {
         if (!mysqli_set_charset($connection, $charset)) {
             log_erro("Erro ao carregar charset $charset: "
                 . mysqli_connect_error());
-            /* trigger_error("Erro ao carregar charset $charset: "
-              . mysqli_connect_error()); */
+           
         }
-    }
+    };
 
     return $connection;
 }
